f9aa5c22ea462cb32b144b670a023bf4baecdffb
beto264 <alb.tejos@gmail.com>
2017-05-04 23:05:06 -0300

    First commit

---
+++ src/main/java/cl/duoc/innovacion/smartshopp/services/Service.java
@@ -0,0 +1,19 @@
+package cl.duoc.innovacion.smartshopp.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class Service {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+		String output = "Jersey say : " + msg;
+		return Response.status(200).entity(output).build();
+	}
+
+}
+
